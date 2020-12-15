b60b6f3db63834a77bbafaf1a05df9e8fbe911cb
Victor Bressani de Mello <victor.bressani@cerc.inf.br>
2020-12-14 23:10:10 -0300

    Gerando Main final

--- src/main/java/br/edu/ufabc/compiladores/isilang/Main.java
+++ src/main/java/br/edu/ufabc/compiladores/isilang/Main.java
@@ -2,43 +2,65 @@ package br.edu.ufabc.compiladores.isilang;
 
 import br.edu.ufabc.compiladores.isilang.antlr4gen.IsiLangLexer;
 import br.edu.ufabc.compiladores.isilang.antlr4gen.IsiLangParser;
-import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
 
     public static void main(final String[] args) throws IOException {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException("Você deve enviar pelo menos uma pasta como argumento!");
+        }
+        final Path file = Path.of(args[0]).toAbsolutePath();
 
-//        if (args.length == 0) {
-//            throw new IllegalArgumentException("Envie um caminho de arquivo como argumento!");
-//        }
-        final Path inputFile = Path.of("C:\\Users\\victo\\Documents\\UFABC\\COMPILADORES\\Compilador-IsiLang\\programa_base.isi").toAbsolutePath();
-        System.out.println(inputFile);
-        final String fileName = inputFile.getFileName()
-                                         .toString()
-                                         .split("\\.")[0];
+        System.out.println(file);
 
-        final InputStream is = new FileInputStream(inputFile.toString());
-        final CharStream charStream = CharStreams.fromStream(is);
+        if (file.toFile().isDirectory()) {
+            Files.list(file)
+                 .forEach(Main::compile);
+            return;
+        }
 
-        final IsiLangLexer lexer = new IsiLangLexer(charStream);
+        compile(file);
+    }
+
+    private static void compile(final Path inputFile) {
+        try {
+            System.out.println("Compilando arquivo: " + inputFile);
+            final String fileName = inputFile.getFileName()
+                                             .toString()
+                                             .split("\\.")[0];
+            final String finalText = compileImpl(inputFile, fileName);
+            saveFinalText(fileName, finalText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String compileImpl(final Path inputFile,
+                                      final String fileName) throws IOException {
+        final IsiLangLexer lexer = new IsiLangLexer(CharStreams.fromStream(new FileInputStream(inputFile.toString())));
         final CommonTokenStream tokens = new CommonTokenStream(lexer);
         final IsiLangParser parser = new IsiLangParser(tokens);
-        ParseTree tree = parser.prog();
+        final ParseTree tree = parser.prog();
 
         final IsiLangJavaVisitor eval = new IsiLangJavaVisitor(fileName);
         eval.visit(tree);
         final String finalText = eval.getFinalText();
+        return finalText;
+    }
 
+    private static void saveFinalText(final String fileName,
+                                      final String finalText) throws IOException {
         final Path finalPath = Path.of(fileName.split("\\.")[0] + ".java");
         Files.write(finalPath, finalText.getBytes());
+        System.out.println("Arquivo compilado e salvo em " + finalPath);
     }
 }
